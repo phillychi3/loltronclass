3b34fcf9a1152ec858393c88906a2a26edbc2f54
白雲 <55632143+phillychi3@users.noreply.github.com>
2023-05-24 19:35:39 +0800

    Create eclass_video_autosetspeed.js

---
+++ eclass_video_autosetspeed.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         eclass autosetspeed
+// @namespace    no
+// @version      0.1
+// @description  no
+// @author       phillychi3
+// @match        https://eclass.yuntech.edu.tw/course/*
+// @match        https://tronclass.com.tw/course/*
+// @icon         https://media.discordapp.net/attachments/922733774633050112/969011518861623376/256.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let video;
+    let speed = 16;
+    document.addEventListener("play", setplayspeed, true);
+    document.addEventListener("playing",whenplaysetspeed,true);
+    function setplayspeed(event) {
+        video = event.target;
+    }
+    function whenplaysetspeed(enent) {
+        event.target.playbackRate = speed;
+        event.target.muted = true;
+        event.target.__proto__.pause = () => {};
+    }
+
+})();
