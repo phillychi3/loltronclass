25a1d2c98ac12b26b8332938b5625e55a78cda20
phillychi3 <phillychi3@gmail.com>
2023-10-30 13:27:23 +0800

    sync

---
+++ tronclass_util.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         tronclass util
+// @namespace    no
+// @version      0.1
+// @description  more useful tools for tronclass
+// @author       lol
+// @match        https://eclass.yuntech.edu.tw/course/
+// @icon         https://media.discordapp.net/attachments/922733774633050112/969011518861623376/256.png
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+// add a card under class="teaching-class-header card"
+
+function makepanel(){
+    let panel = document.createElement("div");
+    panel.innerHTML = `
+
+    `;
+
+
+
+
+    document.querySelector(".teaching-class-header.card")
+
+}
+
+
+
