d2a528985821817351cfdfe5f963045c5af6969f
phillychi3 <phillychi3@gmail.com>
2023-12-03 17:16:54 +0800

    feat: 0.2

--- tronclass_util.js
+++ tronclass_util.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         tronclass util
 // @namespace    no
-// @version      0.1
+// @version      0.2
 // @description  more useful tools for tronclass
 // @author       lol
 // @match        https://eclass.yuntech.edu.tw/course/*
 // @icon         https://media.discordapp.net/attachments/922733774633050112/969011518861623376/256.png
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/phillychi3/loltronclass/main/tronclass_util.js
+// @downloadURL  https://raw.githubusercontent.com/phillychi3/loltronclass/main/tronclass_util.js
 // @license MIT
 // ==/UserScript==
 
@@ -15,13 +17,28 @@
 
 (function () {
   "use strict";
-  function circle_watch() {
+  let tglobal = {
+    process: 0,
+    processmax: 0,
+    persent: 0,
+    ispress: false,
+  };
+
+  function updateprocessbar() {
+    try{
+    let processbar = document.getElementById("watch-process");
+    let persent = (tglobal.process / tglobal.processmax) * 100;
+    processbar.style = `width: ${persent}%`;
+    }catch(e){}
+  }
+
+  function circle_watch(fast = 1000) {
     const video = document.querySelector("video");
     const max = video.duration;
     const maxrun = 60;
-    let videodata = ""
     let lasttime = 0;
     const thisvideoid = document.URL.split("/").splice(-1).toString()
+    tglobal.processmax = max;
     fetch(`https://eclass.yuntech.edu.tw/api/activities/${thisvideoid}`, {
       method: "GET",
       headers: {
@@ -31,22 +48,25 @@
     })
       .then((response) => response.json())
       .then((data) => {
-        videodata = data
         console.log(data)
+        let ct = 0
+        for (let i = 0; i < max; i = i + maxrun) {
+          ct++
+          setTimeout(() => {
+            watchthevideo(i, i + maxrun, data);
+            tglobal.process = i + maxrun;
+            updateprocessbar();
+            if(max-i<maxrun){
+              console.log("last")
+              watchthevideo(i, max, data);
+              tglobal.process = max;
+              updateprocessbar();
+            }
+          }, fast*ct);
+          lasttime = i + maxrun;
+        }
       }
     );
-    console.log(videodata)
-
-    for (let i = 0; i < max; i = i + maxrun) {
-      setTimeout(function () {
-        watchthevideo(i, i + maxrun, videodata);
-      }, 2000);
-      lasttime = i + maxrun;
-    }
-    //無法被整除的部分
-    setTimeout(function () {
-      watchthevideo(lasttime, max, videodata);
-    }, 2000);
   }
 
   function watchthevideo(start, end, videodata) {
@@ -128,8 +148,71 @@
     });
   }
 
-  function makepanel() {
-    console.log("i am in");
+  function makevideopanel() {
+    let panel = document.createElement("div");
+    panel.style = "padding: 20px;margin-top: -40px;";
+    panel.innerHTML = `
+    <div class="panel">
+      <div class="panel-heading">
+        <h4>tronclass util</h4>
+      </div>
+      <div class="panel-body">
+        <div class="panel-buttons" id="buttons">
+          <button class="btn btn-default" id="btn1">watch video</button>
+          <button class="btn btn-default" id="btn2">watch video fast(useless)</button>
+        </div>
+      </div>
+    </div>
+    <style>
+      .panel {
+        margin-bottom: 23px;
+        background: rgba(255, 255, 255, 0.9);
+      }
+      .panel-heading {
+        padding: 0px;
+      }
+    </style>
+    `;
+    document.querySelector('div.fullscreen-right').appendChild(panel);
+    let btn1 = document.getElementById("btn1");
+    btn1.addEventListener("click", function () {
+      if(!tglobal.ispress){
+        let processbar = document.createElement("div");
+        processbar.innerHTML = `
+        <div class="panel-progress">
+            <div class="progress-meter" , id="watch-process" style="width: ${tglobal.persent}%"></div>
+        </div>
+        <style>
+          .panel-progress {
+            margin-top: 20px;
+            padding: 6px;
+            border-radius: 30px;
+            background: rgba(0, 0, 0, 0.25);
+            box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25),
+              0 1px rgba(255, 255, 255, 0.08);
+          }
+          .progress-meter {
+            animation: progressAnimation 6s;
+            background-color: #ef416f;
+            height: 10px;
+            border-radius: 30px;
+            transition: 0.4s ease-out;
+          }
+        `;
+        document.querySelector("div.panel-body").appendChild(processbar);
+        tglobal.ispress = true;
+        circle_watch();
+      }
+    });
+    let btn2 = document.getElementById("btn2");
+    btn2.addEventListener("click", function () {
+      circle_watch(10);
+    });
+  }
+
+
+
+  function makecoursepanel() {
     let panel = document.createElement("div");
     panel.innerHTML = `
     <div class="panel panel-default">
@@ -181,7 +264,11 @@
   function action(observer) {
     observer.disconnect();
     console.log("load done");
-    makepanel();
+    if (document.URL.match(/https?:\/\/eclass.yuntech.edu.tw\/course\/[0-9]{1,6}\/content#\//)) {
+      makecoursepanel();
+    }else if(document.URL.match(/https?:\/\/eclass.yuntech.edu.tw\/course\/[0-9]{1,6}\/learning-activity\/full-screen/)){
+      makevideopanel();
+    }
   }
-  console.log("tronclass util");
+  console.log("%c eclass Util %c https://github.com/phillychi3/loltronclass ", "color: white; background: #e9546b; padding:5px 0;", "padding:4px;border:1px solid #e9546b;");
 })();
