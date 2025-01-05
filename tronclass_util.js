260d93c0d7549977ef29e8808874855027c8d8e2
白雲 <55632143+phillychi3@users.noreply.github.com>
2025-01-05 20:05:19 +0800

    Update tronclass_util.js

--- tronclass_util.js
+++ tronclass_util.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         tronclass util
+// @name         yuntech tronclass util
 // @namespace    no
 // @version      0.3.3
-// @description  more useful tools for tronclass
-// @author       lol
+// @description  一鍵觀看影片，一鍵完成每周影音教材觀看，一鍵觀看投影片
+// @author       someone
 // @match        https://eclass.yuntech.edu.tw/course/*
 // @icon         https://cdn.discordapp.com/avatars/755351137577599016/5ccfb5d525fb3d304c7d61c8d51c6777.png?size=1024
 // @grant        none
